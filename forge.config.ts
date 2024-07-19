dd2a070ef563fcdf975b6924b736b91b9821e8d5
damianof <damianof@users.noreply.github.com>
2024-07-19 10:49:09 -0400

    tweaks

--- forge.config.ts
+++ forge.config.ts
@@ -20,11 +20,11 @@ const config: ForgeConfig = {
       build: [
         {
           // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
-          entry: 'src/main.ts',
+          entry: 'main_electron.ts',
           config: 'vite.main.config.mts'
         },
         {
-          entry: 'src/preload.ts',
+          entry: 'preload.ts',
           config: 'vite.preload.config.mts'
         }
       ],
